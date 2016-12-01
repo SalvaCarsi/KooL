28ab05b45a9dbcd54e5c2d08a7540925d2effca9
salvacarsi <salvavcarsi@gmail.com>
2016-12-01 15:33:02 +0100

    Login with spotify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // TODO move this url to config file
+    const spotifyValidationUrl = 'https://accounts.spotify.com/authorize?client_id=0c1b513f65f54083a4e0fca663fa3693&redirect_uri=http://localhost:3000/&response_type=token';
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +15,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <a href={spotifyValidationUrl}>Login with Spotify!</a>
+        </div>
       </div>
     );
   }
