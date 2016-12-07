b5c668ed65703c7beeb56d2369c940c475d1d1a3
salvacarsi <salvavcarsi@gmail.com>
2016-12-07 17:47:29 +0100

    added new app logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import '../../assets/styles/App.css';
 import '../../assets/styles/navigation.css';
 import NavLink from './helpers/NavLink';
 import { IndexLink } from 'react-router';
+import logo from '../../assets/images/KooL-logo-100px.png';
 
 class Navigation extends Component {
   render() {
     return (
       <div>
+        <img src={logo} alt="logo" className="" />
         <nav role="navigation">
           <ul>
-            <li><IndexLink to="/">KooL Logo</IndexLink></li>
+            <li><IndexLink to="/">Main</IndexLink></li>
             <li><NavLink to="/logged">Logged</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
